238c3a53beb914656b4b0210b779eb0c62f87d91
Hayato <hayato.314.nico@gmail.com>
2023-05-22 21:49:40 +0900

    P.42(CHAPTER1)までの基礎を学習

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 var app = new Vue({
+  //オプション
   el: "#app",
+  data: {
+    message: "初期メッセージ",
+    age: "18",
+    list: ["りんご", "バナナ", "いちご"],
+    show: true,
+  },
+  methods: {
+    handleClick: function (event) {
+      alert(event.target); //[object HTMLButtonElement]
+    },
+  },
 });
+
+console.log(app.message);
+console.log(app.age);
